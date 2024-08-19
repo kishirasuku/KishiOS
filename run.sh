5045b92edca265de9794fc57dc8baba713b4080a
kishirasuku <kishirasuku@gmail.com>
2024-08-19 15:02:04 +0900

    application

--- run.sh
+++ run.sh
@@ -8,11 +8,18 @@ QEMU=qemu-system-riscv32
 # clangのパス (Ubuntuの場合は CC=clang)
 CC=clang
 
+OBJCOPY=/usr/bin/llvm-objcopy
+
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+# シェルをビルド
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # カーネルをビルド
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c shell.bin.o
 
 # QEMUを起動
-$QEMU -machine virt -bios default -serial stdio -display gtk --no-reboot -kernel kernel.elf
+$QEMU -machine virt -bios default  --no-reboot -kernel kernel.elf
 
